3e2665c274291eefe7fbc74546549e1991df42ba
Jae <90853880+javerinetan@users.noreply.github.com>
2023-10-24 13:55:29 +0800

    Update test.py

--- Linear/test.py
+++ Linear/test.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
-from models.linear_regression import LinearRegression
+from linear_regression import LinearRegression
 
 
 X , y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
